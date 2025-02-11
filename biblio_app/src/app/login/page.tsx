728099ca4457e1e0cc9ce4e3c80e17b8abca48e1
gustavo de assis <gustavo.siqueira.421@gmail.com>
2025-02-11 12:43:16 -0300

    feat: added background image at login page

--- biblio_app/src/app/login/page.tsx
+++ biblio_app/src/app/login/page.tsx
@@ -3,7 +3,12 @@ import LoginForm from "@/components/loginForm";
 export default function Login() {
   return (
     <main className="flex flex-row w-full h-full">
-      <section className="bg-red-300 h-screen w-2/3">a</section>
+      <section className="h-screen w-2/3">
+        <div
+          className="fixed top-0 left-0 w-full h-full bg-cover bg-no-repeat -z-10"
+          style={{ backgroundImage: `url('/assets/images/biblioteca.png')` }}
+        ></div>
+      </section>
       <section className="flex flex-col justify-center items-center bg-blue-400 h-screen w-1/3 gap-6">
         <div className="text-4xl">Bem vindo à biblioteca ADJQ</div>
         <LoginForm />
